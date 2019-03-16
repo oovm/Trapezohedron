b0fe7817f79ec6744b63155be04866b2da14ce86
Aster <galaster@foxmail.com>
2019-03-16 23:13:27 +0800

    MathematicaV4::fix legacy functions

--- projects/MathematicaV5/source.m
+++ projects/MathematicaV5/source.m
@@ -78,7 +78,10 @@ PolyhedronData["Dodecahedron"]
 
 SolidToTriangles[solid_, l_ : 5] := Block[
 	{g},
-	g[p_] := {Polygon[{#[[1]], #[[2]], Plus @@ p[[1]] / l}]}& /@ Partition[Append[p[[1]], First[p[[1]]]], 2, 1];
+	g[p_] := Map[
+		{Polygon[{#[[1]], #[[2]], Plus @@ p[[1]] / l}]}& ,
+		Partition[Append[p[[1]], First[p[[1]]]], 2, 1]
+	];
 	Flatten[g /@ Flatten@PolyhedronData[solid, "Faces", "Polygon"]]
 ];
 
@@ -121,6 +124,7 @@ TriangulatedTriangle = Polygon[Append[#, 0]& /@ #]& /@ ({
 	{{4, -6}, {5, -5}, {6, -6}}
 } / 6);
 
+
 Graphics3D[TriangulatedTriangle]
 
 
@@ -128,12 +132,20 @@ Graphics3D[TriangulatedTriangle]
 (*The function MapTo3DTriangle maps this triangulation onto all triangles of the above triangulation of the dodecahedron.*)
 
 
-MapTo3DTriangle[Polygon[{p1_, p2_, p3_}]] := Module[{mp = (p1 + p2) / 2, dirx, diry}, diry = p3 - mp;dirx = p1 - mp;Map[p3 + #[[1]] dirx + #[[2]] diry&, N[TriangulatedTriangle], {-2}]]
+MapTo3DTriangle[Polygon[{p1_, p2_, p3_}]] := Module[
+	{mp = (p1 + p2) / 2, dirx, diry},
+	diry = p3 - mp;
+	dirx = p1 - mp;
+	Map[
+		p3 + #[[1]] dirx + #[[2]] diry&,
+		N[TriangulatedTriangle], {-2}
+	]
+];
 
 
 Graphics3D[
 	Subscript[\[ScriptH], 2] = Polygon /@ (N[First /@ Flatten[MapTo3DTriangle /@ Take[Subscript[\[ScriptH], 1], All]]]),
-	PlotRange -> All, Axes -> False
+	PlotRange -> All, Axes -> False, Boxed -> False
 ]
 
 
@@ -145,13 +157,20 @@ Graphics3D[
 (*The function TriangleBorders constructs the outlines of all triangles.*)
 
 
-TriangleBorders[{plato_, lateralContractionFactor_, {\[Delta]ppl_, ppq_}}, Polygon[{p1_, p2_, p3_}]] :=
-	Module[{mp = (p1 + p2 + p3) / 3, p1i, p2i, p3i, ppl}, {p1i, p2i, p3i} = (mp + lateralContractionFactor (# - mp)&) /@ {p1, p2, p3};
+TriangleBorders[{plato_, lateralContractionFactor_, {\[Delta]ppl_, ppq_}}, Polygon[{p1_, p2_, p3_}]] := Module[
+	{mp = (p1 + p2 + p3) / 3, p1i, p2i, p3i, ppl, f, g},
+	{p1i, p2i, p3i} = (mp + lateralContractionFactor (# - mp)&) /@ {p1, p2, p3};
 	ppl = Ceiling[1 / \[Delta]ppl Max[Sqrt[#.#]& /@ Apply[Subtract, Partition[{p1i, p2i, p3i, p1i}, 2, 1], {1}]]];
-	{Apply[Function[{q1, q1i, q2i, q2}, Function[pn, Table[Polygon[{pn[[i, j]], pn[[i + 1, j]], pn[[i + 1, j + 1]], pn[[i, j + 1]]}], {i, ppl}, {j, ppq}]][Table[q1 + (ix (q2 - q1)) / ppl + ((1 - ix / ppl) iy (q1i - q1)) / ppq + (ix iy (q2i - q2)) / (ppl ppq), {ix, 0, ppl}, {iy, 0, ppq}]]], {{p1, p1i, p2i, p2}, {p2, p2i, p3i, p3}, {p3, p3i, p1i, p1}}, {1}], Line[Join @@ (Table[#[[1]] + (i (#[[2]] - #[[1]])) / ppl, {i, 0, ppl}]&) /@ {{p1i, p2i}, {p2i, p3i}, {p3i, p1i}}]}]
+	f[pn_] := Table[Polygon[{pn[[i, j]], pn[[i + 1, j]], pn[[i + 1, j + 1]], pn[[i, j + 1]]}], {i, ppl}, {j, ppq}];
+	g[q1_, q1i_, q2i_, q2_] := f[Table[q1 + (ix (q2 - q1)) / ppl + ((1 - ix / ppl) iy (q1i - q1)) / ppq + (ix iy (q2i - q2)) / (ppl ppq), {ix, 0, ppl}, {iy, 0, ppq}]];
+	{
+		Apply[g, {{p1, p1i, p2i, p2}, {p2, p2i, p3i, p3}, {p3, p3i, p1i, p1}}, {1}],
+		Line[Join @@ (Table[#[[1]] + (i (#[[2]] - #[[1]])) / ppl, {i, 0, ppl}]&) /@ {{p1i, p2i}, {p2i, p3i}, {p3i, p1i}}]
+	}
+];
 
 
-Show[Graphics3D[{EdgeForm[], Subscript[\[ScriptH], 3] = Transpose[TriangleBorders[{"Dodecahedron", 0.8, {0.08, 1}}, #]& /@ Subscript[\[ScriptH], 2]]}]];
+Graphics3D[{EdgeForm[], Subscript[\[ScriptH], 3] = Transpose[TriangleBorders[{"Dodecahedron", 0.8, {0.08, 1}}, #]& /@ Subscript[\[ScriptH], 2]]}]
 
 
 (* ::Section:: *)
@@ -162,15 +181,15 @@ Show[Graphics3D[{EdgeForm[], Subscript[\[ScriptH], 3] = Transpose[TriangleBorder
 (*The next task is to create a function for calculating the radial transformation.*)
 
 
+
+
+
 attract[r_] := Re[1 / 2 (ArcSin[2 r - 1] + \[Pi] / 2)];
-makeContractFunction[] := (
-	contract[x_] = Module[
-		{xMin, xMax, xm, dx},
-		{xMax, xMin} = Sqrt[({Max[#], Min[#]}&)[(#.#&) /@ Level[Subscript[\[ScriptH], 2], {-2}]]];
-		attract[(x - xMin) / (xMax - xMin)]
-	]
-);
-makeContractFunction[];
+contract[x_] := Module[
+	{xMin, xMax, xm, dx},
+	{xMax, xMin} = Sqrt[({Max[#], Min[#]}&)[(#.#&) /@ Level[Subscript[\[ScriptH], 2], {-2}]]];
+	attract[(x - xMin) / (xMax - xMin)]
+];
 contract[x_List] := (x contract[Sqrt[x.x]]) / Sqrt[x.x];
 
 
@@ -178,21 +197,38 @@ contract[x_List] := (x contract[Sqrt[x.x]]) / Sqrt[x.x];
 (*The following is a plot of the function contract.*)
 
 
-Plot[contract[x], {x, 0.7, 1.2}, Frame -> True, Axes -> False, PlotRange -> All];
+Plot[contract[x], {x, 0.7, 1.2}, Frame -> True, Axes -> False, PlotRange -> All]
 
 
 (* ::Text:: *)
 (*Applying contract to Subscript[\[ScriptH], 2] gives the following hyperbolic dodecahedron.*)
 
 
-Show[Graphics3D[Map[contract, Subscript[\[ScriptH], 2], {-2}]], PlotRange -> All, Axes -> False];
+Graphics3D[
+	Map[contract, Subscript[\[ScriptH], 2], {-2}],
+	PlotRange -> All, Axes -> False
+]
 
 
 (* ::Text:: *)
 (*The function Hyperbolicize takes the triangle outlines in Subscript[\[ScriptH], 3] and applies the function contract. In addition, it does this with a smaller copy of Subscript[\[ScriptH], 3] and connects the edges of the resulting two hyperbolic dodecahedra.*)
 
 
-Hyperbolicize[f_, {polys_, edges_}, radialContractionFactor_] := Module[{l = First /@ Map[f, edges, {-2}], p = First /@ Map[f, Flatten[polys], {-2}], outerPolys, innerPolys, radialPolys, outerEdges, innerEdges}, outerPolys = Polygon /@ p;innerPolys = Polygon /@ Map[radialContractionFactor #1&, p, {-2}];radialPolys = Polygon /@ (Join[#[[1]], Reverse[#[[2]]]]&) /@ Transpose[{Flatten[(Partition[#, 2, 1]&) /@ l, 1], Flatten[(Partition[#1, 2, 1]&) /@ Map[radialContractionFactor #1&, l, {-2}], 1]}];{outerPolys, innerPolys, radialPolys}];
+Hyperbolicize[f_, {polys_, edges_}, radialContractionFactor_] := Module[
+	{
+		l = First /@ Map[f, edges, {-2}],
+		p = First /@ Map[f, Flatten[polys], {-2}],
+		Polys, outerPolys, innerPolys, radialPolys, outerEdges, innerEdges
+	},
+	Polys = Transpose@{
+		Flatten[(Partition[#, 2, 1]&) /@ l, 1],
+		Flatten[(Partition[#1, 2, 1]&) /@ Map[radialContractionFactor #1&, l, {-2}], 1]
+	};
+	outerPolys = Polygon /@ p;
+	innerPolys = Polygon /@ Map[radialContractionFactor #1&, p, {-2}];
+	radialPolys = Polygon /@ (Join[#[[1]], Reverse[#[[2]]]]&) /@ Polys ;
+	{outerPolys, innerPolys, radialPolys}
+];
 
 
 (* ::Text:: *)
@@ -202,7 +238,13 @@ Hyperbolicize[f_, {polys_, edges_}, radialContractionFactor_] := Module[{l = Fir
 Subscript[\[ScriptH], 4] = Hyperbolicize[contract, Subscript[\[ScriptH], 3], 0.9];
 
 
-CoverGraphicsMathematicaV5 = Show[Graphics3D[{EdgeForm[], SurfaceColor[Hue[0.08], Hue[0.12], 2.4], Subscript[\[ScriptH], 4]}], PlotRange -> All, Boxed -> False];
+Graphics3D[{
+	EdgeForm[],
+	SurfaceColor[Hue[0.08], Hue[0.12], 2.4],
+	Flatten@Subscript[\[ScriptH], 4]
+},
+	PlotRange -> All, Boxed -> False
+]
 
 
 (* ::Section:: *)
@@ -213,14 +255,18 @@ CoverGraphicsMathematicaV5 = Show[Graphics3D[{EdgeForm[], SurfaceColor[Hue[0.08]
 (*The above implementation can be used with other Platonic solids, radial transformations, and coloring schemes. Here it is done with a cube (hexahedron).*)
 
 
-Subscript[\[ScriptH], 1] = SolidToTriangles[Hexahedron];
+Subscript[\[ScriptH], 1] = SolidToTriangles["Hexahedron"];
 Subscript[\[ScriptH], 2] = Polygon /@ (N[First /@ Flatten[MapTo3DTriangle /@ Take[Subscript[\[ScriptH], 1], All]]]);
 Subscript[\[ScriptH], 3] = Transpose[TriangleBorders[{Hexahedron, 0.8, {0.08, 1}}, #]& /@ Subscript[\[ScriptH], 2]];
-makeContractFunction[];
 Subscript[\[ScriptH], 4] = Hyperbolicize[contract, Subscript[\[ScriptH], 3], 0.9];
 
 
-rainbowColor[p : Polygon[l_]] := {(SurfaceColor[#, #, 2.2]&)[Hue[2Sqrt[#.#]&[Plus @@ l / Length[l]] / \[Pi]]], p}
+rainbowColor[p : Polygon[l_]] := {(SurfaceColor[#, #, 2.2]&)[Hue[2Sqrt[#.#]&[Plus @@ l / Length[l]] / \[Pi]]], p};
 
 
-Show[Graphics3D[{EdgeForm[], rainbowColor /@ Flatten[Subscript[\[ScriptH], 4]]}], PlotRange -> All, Boxed -> False];
+Graphics3D[{
+	EdgeForm[],
+	rainbowColor /@ Flatten[Subscript[\[ScriptH], 4]]
+},
+	PlotRange -> All, Boxed -> False
+]
